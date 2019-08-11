99710a3cd83c3fa4d6d1c7a30c3e81a57d6d0b83
Alan Giojelli <alangiojelli@Alans-MacBook-Air.local>
2019-08-10 22:06:45 -0300

    First PDF Renderer

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,14 @@
 class ReportsController < ApplicationController
+  include ActionController::MimeResponds
   def index
+    respond_to do |format|
+
+      format.pdf do
+
+        render pdf: "pdf_name"
+
+      end
+
+    end
   end
 end
