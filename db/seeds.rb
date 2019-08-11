ebb775bd3edf61f77dce69773fa00d04790d1662
Alan Giojelli <alangiojelli@Alans-MacBook-Air.local>
2019-08-11 02:29:54 -0300

    Model seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read('lib/input/formulations.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+Formulation.create(row.to_hash)
+end
+
+csv_text = File.read('lib/input/ingredients.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+Ingredient.create(row.to_hash.slice('name', 'minimum_percentage', 'maximum_percentage', 'description'))
+end
+
+csv_text = File.read('lib/input/formulation_ingredients.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+FormulationsIngredient.create(row.to_hash)
+end
