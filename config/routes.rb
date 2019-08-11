d5acac2a020b8baf30ec0158e261b05c09cb02e6
Alan Giojelli <alangiojelli@Alans-MacBook-Air.local>
2019-08-11 02:45:37 -0300

    Added Missing Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :reports do
   end
+  resources :formulations do
+  end
+  resources :ingredients do
+  end
 end
